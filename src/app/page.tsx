0a1f6ed0b8d60a9f9aca0b02438dec4cde909ec2
Rafael Damasco <rdamasco@mcfadyen.com>
2023-03-03 10:43:04 -0300

    feat: skeleton screen on loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import fetchNews from 'utils/fetchNews';
 import { categories } from '../../constants';
 import NewsList from './NewsList';
 import response from 'response.json';
+import Loading from './loading';
 
 export default async function Homepage() {
   const news: NewsResponse = await fetchNews(categories.join(','));
 
   return (
     <div>
+      <Loading />
       <NewsList news={news} />
     </div>
   );
